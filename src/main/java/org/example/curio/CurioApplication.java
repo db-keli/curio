321af8993cfad28357762c1875137f52bc16a773
kekeli <kekelidompeh@gmail.com>
2026-02-17 15:18:44 +0000

    Initial commit: Spring Boot Curio application

---
+++ src/main/java/org/example/curio/CurioApplication.java
@@ -0,0 +1,13 @@
+package org.example.curio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/curio/CurioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.curio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
